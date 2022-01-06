387054bda45670eac9d4955411b3535cd5f7ee9e
Gleb <glebdvp@gmail.com>
2022-01-06 21:58:15 +0700

    Добавлена форма RSVP, методы для HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
 namespace PartInvites.Controllers
 {
     public class HomeController : Controller
@@ -8,6 +10,19 @@ namespace PartInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
+        }
+        // атрибут указывает MVC, что метод должен использоваться только для GET запросов
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        // атрибут указывает MVC, что метод должен использоваться только для POST запросов
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TO DO: Save data from guestResponse
+            return View();
         }
     }
 }
