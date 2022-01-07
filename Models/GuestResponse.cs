ef1b2fdcbd3a670f0d066aa7c10a764132c85832
Gleb <glebdvp@gmail.com>
2022-01-08 03:47:29 +0700

    Добавлена стилизация страниц

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -8,7 +8,7 @@ namespace PartyInvites.Models
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter your email address")]
-        [RegularExpression(".+\\@.+\\..",
+        [RegularExpression(".+\\@.+\\..+",
              ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter your phone number")]
